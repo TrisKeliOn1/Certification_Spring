66eb63dfd22f4a934a6ff7ee241d196119880d57
Hebert <hebertLandim007@outlook.com>
2024-02-09 17:10:27 -0300

    finished

--- src/main/java/com/rocketseat/certification_nlw/modules/students/entities/StudentEntity.java
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/entities/StudentEntity.java
@@ -1,15 +1,23 @@
 package com.rocketseat.certification_nlw.modules.students.entities;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -17,8 +25,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity(name = "students")
+@Builder
 public class StudentEntity {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
@@ -26,6 +35,16 @@ public class StudentEntity {
     @Column(unique = true, nullable = false)
     private String email;
 
+    // OneToOne
+    // OneToMany
+    // ManyToOne
+    // ManyToMany
+
     @OneToMany(mappedBy = "studentEntity")
+    @JsonBackReference
     private List<CertificationStudentEntity> certificationStudentEntity;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
 }
